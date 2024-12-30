7948a13a3d8f9864b36f85657425b207b77083e3
luzdl <luz-de-leon@outlook.com>
2024-12-30 17:23:28 -0500

    admin routes & controller

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
-import { getAdmin } from "../controllers/admin.controller";
+import { checkAdmin, createSong, deleteSong, createAlbum, deleteAlbum } from "../controller/admin.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", getAdmin);
+//checks if the user is logged in and is an admin before accesing the other routes
+router.use(protectRoute, requireAdmin);
+
+router.get("/check", checkAdmin);
+
+router.post("/songs", createSong);
+router.delete("/songs/:id", deleteSong);
+
+router.post("/albums", createAlbum);
+router.delete("/albums/:id", deleteAlbum);
 
 export default router;
