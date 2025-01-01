3b4a809ddc71aacad044910fef0a96a92e8fab55
luzdl <luz-de-leon@outlook.com>
2025-01-01 05:10:21 -0500

    MainLayout and LeftSidebar

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,19 @@
+export interface Song {
+    _id: string;
+    title: string;
+    artist: string;
+    albumId: string | null;
+    imageUrl: string;
+    audioUrl: string;
+    duration: Date;
+    createdAt: Date;
+}
+
+export interface Album {
+    _id: string;
+    title: string;
+    artist: string;
+    imageUrl: string;
+    releaseYear: number;
+    songs: Song[];
+}
