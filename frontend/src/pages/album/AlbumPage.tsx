27211b1545cb20810e887d30d1dee4cd8fc8e575
luzdl <luz-de-leon@outlook.com>
2025-03-01 06:21:33 -0500

    playback controls

--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -8,7 +8,7 @@ import { usePlayerStore } from "@/stores/usePlayerStore";
 
 export const formatDuration = (seconds: number) => {
 	const minutes = Math.floor(seconds / 60);
-	const remainingSeconds = seconds % 60;
+	const remainingSeconds = Math.floor(seconds % 60);
 	return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
 };
 
