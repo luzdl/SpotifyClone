17d3ad9d9e904caf5779cf9bb4a58541d9333a92
luzdl <luz-de-leon@outlook.com>
2025-03-01 22:27:14 -0500

    deployment setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import fileUpload from "express-fileupload"
 import path from 'path';
 import cors from 'cors';
 import {createServer} from "http";
+import cron from "node-cron";
+import fs from "fs";
 
 import { initializeSocket } from './lib/socket.js';
 
@@ -44,12 +46,34 @@ app.use(fileUpload({
     })
 );
 
+const tempDir = path.join(process.cwd(), "tmp");
+cron.schedule("0 * * * *", () => {
+	if (fs.existsSync(tempDir)) {
+		fs.readdir(tempDir, (err, files) => {
+			if (err) {
+				console.log("error", err);
+				return;
+			}
+			for (const file of files) {
+				fs.unlink(path.join(tempDir, file), (err) => {});
+			}
+		});
+	}
+});
+
 app.use("/api/users", usersRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/api/auth", authRoutes); // Ensure this line is present
+app.use("/api/auth", authRoutes); 
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+	});
+}
 
 //error handler
 app.use((err, req, res, next) => {
